bcf5bb04f205e3336cfe5fb0aedc71347e1d074f
nisa <nisa@local.fr>
2020-10-03 21:29:34 +0700

    install cors, enable cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express();
 const port = 3004;
+const cors = require('cors');
 const site = require('./controllers/SiteController'),
       customer = require('./controllers/CustomerController');
 
 app.locals.author = 'dreas';
+app.use(cors());
 app.use('/', site);
 app.use('/customer', customer);
 
