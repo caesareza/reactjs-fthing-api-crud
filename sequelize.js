9858b08b98a5b3c82413024151b9abfdbae45e13
nisa <nisa@local.fr>
2020-10-05 09:13:50 +0700

    env

--- sequelize.js
+++ sequelize.js
@@ -1,8 +1,9 @@
 const Sequelize = require('sequelize');
 const CustomerModel = require('./models/Customer');
+require('dotenv').config();
 
-const sequelize = new Sequelize('', '', '', {
-    host: '',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     define: {
         underscored: true,
